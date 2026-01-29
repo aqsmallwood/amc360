42a61997547c0293ed17969e896255c4ea79ab36
Adrian Smallwood <smallwoodadrian54@gmail.com>
2026-01-28 20:26:55 -0700

    redesign

--- dist/_astro/hoisted.6119e170.js
+++
@@ -1,1 +0,0 @@
-import"https://identity.netlify.com/v1/netlify-identity-widget.js";window.netlifyIdentity&&window.netlifyIdentity.on("init",i=>{i||window.netlifyIdentity.on("login",()=>{document.location.href="/admin/"})});
